eae27b2ccf9a7f464ac360ad952b5e5841853b0b
Aila Bogasieru <aila.bogasieru@agys.ch>
2019-01-22 13:18:24 +0200

    fix some POST tests

--- src/test/java/com/APISmartFabric/Tests/GUIControlPortalRest/GET/GUI_Portal_Detailed_ListTest35.java
+++ src/test/java/com/APISmartFabric/Tests/GUIControlPortalRest/GET/GUI_Portal_Detailed_ListTest35.java
@@ -9,35 +9,32 @@ import java.io.FileNotFoundException;
 
 /**
  * 
-@author aila.bogasieru@agys.ch
+ * @author aila.bogasieru@agys.ch
  *
  */
 
-
 public class GUI_Portal_Detailed_ListTest35 {
-	
-	private static final Logger logger = LoggerFactory.getLogger(GUI_Portal_Detailed_ListTest35.class);
 
+	private static final Logger logger = LoggerFactory.getLogger(GUI_Portal_Detailed_ListTest35.class);
 
 	@Test
 	public void getGUIPortalDEtailedList() throws FileNotFoundException {
 		given().header("principal",
 				"{ \"tenantId\": \"d634b20d-128e-4a57-97cf-7b7b01aeb901\", \"tenantDomain\": \"DTSW\", \"userId\": \"2c39c58f-b4a5-40a9-9826-9dce8b57a2fa\", \"userEmail\": \"test_user@agys.ch (test_user@agys.ch)\", \"language\": null, \"userFirstName\": null, \"userLastName\": null, \"permissions\": [] }")
-				.when().contentType("application/json").get(CredentialsUtils.getProperty("baseURLGUI")
+				.when().contentType("application/json")
+				.get(CredentialsUtils.getProperty("baseURLGUI")
 						+ CredentialsUtils.getProperty("middleURLGUIPortalDetailedList"))
 				.then().assertThat().statusCode(200);
 		logger.info("GUI_Portal_Detailed_List" + CredentialsUtils.getProperty("baseURL")
 				+ CredentialsUtils.getProperty("middleURLGUIPortalDetailedList"));
 	}
 
-	
 	@Test
 	public void getGUIPortalDEtailedListNoAuthentication() throws FileNotFoundException {
-		given()
-				.when().contentType("application/json").get(CredentialsUtils.getProperty("baseURLGUI")
+		given().when().contentType("application/json")
+				.get(CredentialsUtils.getProperty("baseURLGUI")
 						+ CredentialsUtils.getProperty("middleURLGUIPortalDetailedList"))
 				.then().assertThat().statusCode(403);
-		.then().assertThat().statusCode(200);
 		logger.info("GUI_Portal_Detailed_List" + CredentialsUtils.getProperty("baseURL")
 				+ CredentialsUtils.getProperty("middleURLGUIPortalDetailedList"));
 	}
--- src/test/java/com/APISmartFabric/Tests/tenantController/POST/Idenity_Tenants_UsersTest24.java
+++ src/test/java/com/APISmartFabric/Tests/tenantController/POST/Idenity_Tenants_UsersTest24.java
@@ -6,19 +6,15 @@ import org.testng.annotations.Test;
 import com.APISmartFabric.Utils.CredentialsUtils;
 import com.APISmartFabric.Utils.RensposeBodyDisplay;
 import com.APISmartFabric.controller.AdminController.CreateUserRequest;
-import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.response.Response;
-import com.jayway.restassured.specification.RequestSpecification;
 import static com.jayway.restassured.RestAssured.given;
 
 import java.util.UUID;
 
 public class Idenity_Tenants_UsersTest24 {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(Idenity_Tenants_UsersTest24.class);
 
 	final String code = "35";
-	final String id = "45654";
 	final String email = UUID.randomUUID() + "@testfabric.ch";
 	final String firstName = "Aila";
 	final String lastName = "Bogasieru";
@@ -31,10 +27,7 @@ public class Idenity_Tenants_UsersTest24 {
 	final String token = "365456";
 	final String tokenExpiry = "2019-01-11T13:31:19.825Z";
 
-	private String baseURL = "http://dev01.hq.smartfabric.ch:7000";
-	RequestSpecification httpRequest = RestAssured.given();
-	Response response = httpRequest.post(baseURL + "/identity/admin/users");
-	String location = response.header("location");
+
 	CreateUserRequest createUser = new CreateUserRequest(language, lastName, password, phone, title, token, tokenExpiry,
 			code, firstName, gender, email, status);
 
@@ -43,14 +36,17 @@ public class Idenity_Tenants_UsersTest24 {
 		given().header("principal",
 				"{ \"tenantId\": \"d634b20d-128e-4a57-97cf-7b7b01aeb901\", \"tenantDomain\": \"DTSW\", \"userId\": \"2c39c58f-b4a5-40a9-9826-9dce8b57a2fa\", \"userEmail\": \"test_user@agys.ch (test_user@agys.ch)\", \"language\": null, \"userFirstName\": null, \"userLastName\": null, \"permissions\": [] }")
 				.contentType("application/json")
-				.body("{\"code\":\"" + createUser.getCode() + "\",\n" + "\"email\":\"" + createUser.getEmail() + "\", \n"
-						+ "\"firstName\":\"" + createUser.getFirstName() + "\", \n" + "\"gender\":\"" + createUser.getGender()
-						+ "\", \n" + "\"id\":\"" + id + "\", \n" + "\"language\":\"" + createUser.getLanguage() + "\", \n"
-						+ "\"password\":\"" + createUser.getPassword() + "\", \n" + "\"phone\":\"" + createUser.getPhone()
+				.body("{\"code\":\"" + createUser.getCode() + "\",\n" + "\"email\":\"" + createUser.getEmail()
+						+ "\", \n" + "\"firstName\":\"" + createUser.getFirstName() + "\", \n" + "\"lastName\":\""
+						+ createUser.getLastName() + "\", \n" + "\"gender\":\"" + createUser.getGender() + "\", \n"
+						+ "\"language\":\"" + createUser.getLanguage() + "\", \n" + "\"lastName\":\""
+						+ createUser.getLastName() + "\", \n" + "\"password\":\"" + createUser.getPassword() + "\", \n"
+						+ "\"phone\":\"" + createUser.getPhone() + "\", \n" + "\"phone\":\"" + createUser.getPhone()
 						+ "\", \n" + "\"status\":\"" + createUser.getStatus() + "\", \n" + "\"title\":\""
 						+ createUser.getTitle() + "\", \n" + "\"token\":\"" + createUser.getToken() + "\", \n"
-						+ "\"tokenExpiry\":\"" + createUser.getTokenExpiry() + "\" }")
-				.when().post(CredentialsUtils.getProperty("baseURL") + CredentialsUtils.getProperty("middleURLUser"))
+						+ "\"title\":\"" + createUser.getTitle() + "\", \n" + "\"token\":\"" + createUser.getToken()
+						+ "\", \n" + "\"tokenExpiry\":\"" + createUser.getTokenExpiry() + "\" }")
+				.when().post(CredentialsUtils.getProperty("baseURL") + CredentialsUtils.getProperty("middleURLTenantsUsers"))
 				.then().statusCode(201);
 		RensposeBodyDisplay responseR = new RensposeBodyDisplay();
 		logger.info("Response body" + responseR.response());
@@ -61,14 +57,17 @@ public class Idenity_Tenants_UsersTest24 {
 		given().header("principal",
 				"{ \"tenantId\": \"d634b20d-128e-4a57-97cf-7b7b01aeb901\", \"tenantDomain\": \"DTSW\", \"userId\": \"2c39c58f-b4a5-40a9-9826-9dce8b57a2fa\", \"userEmail\": \"test_user@agys.ch (test_user@agys.ch)\", \"language\": null, \"userFirstName\": null, \"userLastName\": null, \"permissions\": [] }")
 				.contentType("application/json")
-				.body("{\"code\":\"" + createUser.getCode() + "\",\n" + "\", \n" + "\"firstName\":\""
-						+ createUser.getFirstName() + "\", \n" + "\"gender\":\"" + createUser.getGender() + "\", \n"
-						+ "\"id\":\"" + id + "\", \n" + "\"language\":\"" + createUser.getLanguage() + "\", \n"
-						+ "\"password\":\"" + createUser.getPassword() + "\", \n" + "\"phone\":\"" + createUser.getPhone()
+				.body("{\"code\":\"" + createUser.getCode() + "\",\n" + "\"email\":\"" + createUser.getEmail()
+						+ "\", \n" + "\"firstName\":\"" + createUser.getFirstName() + "\", \n" + "\"lastName\":\""
+						+ createUser.getLastName() + "\", \n" + "\"gender\":\"" + createUser.getGender() + "\", \n"
+						+ "\"language\":\"" + createUser.getLanguage() + "\", \n" + "\"lastName\":\""
+						+ createUser.getLastName() + "\", \n" + "\"password\":\"" + createUser.getPassword() + "\", \n"
+						+ "\"phone\":\"" + createUser.getPhone() + "\", \n" + "\"phone\":\"" + createUser.getPhone()
 						+ "\", \n" + "\"status\":\"" + createUser.getStatus() + "\", \n" + "\"title\":\""
 						+ createUser.getTitle() + "\", \n" + "\"token\":\"" + createUser.getToken() + "\", \n"
-						+ "\"tokenExpiry\":\"" + createUser.getTokenExpiry() + "\" }")
-				.when().post(CredentialsUtils.getProperty("baseURL") + CredentialsUtils.getProperty("middleURLUser"))
+						+ "\"title\":\"" + createUser.getTitle() + "\", \n" + "\"token\":\"" + createUser.getToken()
+						+ "\", \n" + "\"tokenExpiry\":\"" + createUser.getTokenExpiry() + "\" }")
+				.when().post(CredentialsUtils.getProperty("baseURL") + CredentialsUtils.getProperty("middleURLTenantsUsers"))
 				.then().statusCode(404);
 		RensposeBodyDisplay responseR = new RensposeBodyDisplay();
 		logger.info("Response body" + responseR.response());
